9e240e1b889e64525d444bcde7cb461572f61342
Altora Mclean <altoramclean@gmail.com>
2021-11-03 15:09:48 -0400

    app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,32 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
-import React, {useEffect, useState} from "react";
 import axios from 'axios';
-import Image from './components/Image';
+import Title from './components/Title';
+import Img from './components/Image';
 import Explanation from './components/Explanation';
 import ExtraStuff from './components/extraStuff';
 
 function App() {
+const [nasa, setNasa] = useState([])
+  useEffect(() => {
+    axios.get(`https://api.nasa.gov/planetary/apod?api_key=9AbzILPniiieYHA8MPMNrbmTk5y1jqh9t0svIYkq`)
+    .then(res => {
+      setNasa(res.data)
+    })
+    .catch(error => {
+      console.error(error)
+    })
+
+  },[])
+  console.log(nasa)
+
+
+
+
+
   return (
     <div className="App">
-      <p>
-        Read through the instructions in the README.md file to build your NASA
-        app! Have fun <span role="img" aria-label='go!'>🚀</span>!
-      </p>
+      <h1> Welcome to the NASA photo of the day!</h1>
     </div>
   );
 }
